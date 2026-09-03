e0dd5b98af0ba044cd29142697277e7ac071b221
Juliana Mininel <57683000+jmininel@users.noreply.github.com>
2026-09-03 18:49:36 +0000

    "update:  insert components into page"

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
+import Banner from "./_components/Banner/Banner";
+import CarouselImages from "./_components/Carousel/CarouselImage";
+import Footer from "./_components/Footer/Footer";
 import { Header } from "./_components/Header/Header";
+import { Hero } from "./_components/Hero/Hero";
 
 export default function Home() {
   return (
  <div>
   <Header/>
+  <Hero/>
+  <CarouselImages/>
+  <Banner/>
+  <Footer/>
  </div>
   );
 }
